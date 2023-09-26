db38dcedebb486ecf48ad11c9f6b2bd8cbfbfa36
rick <rickyamas@gmail.com>
2023-09-26 14:16:45 -0300

    Detona Ralph:implementando as views e values

--- Detona Ralph com JavaScript/src/scripts/engine.js
+++ Detona Ralph com JavaScript/src/scripts/engine.js
@@ -1,1 +1,55 @@
-alert("Hello world")
+const state = {
+    view:{
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+  
+    values: {
+        timeId:null
+        gameVelocity: 1000
+    },
+};
+
+// vai fazer ele mover a cada x tempo
+function moveEnemy(){
+    state.values.timeId = setInterval(randomSquare, state.values.gameVelocity);
+}
+
+
+// escolher qual quadrante que ele vai aparecer
+function randomSquare(){
+
+    //limpando: remover toda classe enemy
+    state.view.squares.forEach((squares) => {
+        squares.classList.remove("enemy");
+    });
+
+    //sotear
+    let randomNumber = Math.floor(Math.random()*9);
+
+    // pegando o id
+    let randomSquare = state.view.squares[randomNumber];
+
+    //adicionado classe enemy
+    randomSquare.classList.add("enemy");
+}
+
+
+
+
+function addListenerHitBox(){
+    state.views.squares.forEach((square) => {
+        if(square.id === 1)
+            console.log("olha");
+    });
+}
+
+
+function initialize(){
+    moveEnemy();
+}
+
+
+initialize();
