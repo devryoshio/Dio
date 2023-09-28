c435315ca80b297ca2aca59566278ed1d961f338
rick <rickyamas@gmail.com>
2023-09-28 17:10:47 -0300

    finish: jogo da memoria com emojis uti....

---
+++  Jogo da memória com Emojis Utilizando Javascript/README.md
@@ -0,0 +1,17 @@
+# Introdução 
+
+È um jogo bacana, onde aprendemos varios conceitos importante de JS.
+
+
+# Agracimento 
+
+Ao professor Felipão!
+
+
+# Melhoria no futuro
+
+- [ ] Criar um banco de dados para guardar Ranking
+- [ ] Melhorar o desempenho da pontuação
+- [ ] colocar tempo;
+- [ ] Colocar level, cada vez fica mais dificil tanto em relaçao ao tempo e quantidade emoji
+- [ ] Fucionamento em Firefox, me parece que não reconhece inset do CSS.
---  Jogo da memória com Emojis Utilizando Javascript/src/scripts/engine.js
+++  Jogo da memória com Emojis Utilizando Javascript/src/scripts/engine.js
@@ -1,11 +1,10 @@
-const emojis = ["🐵", "🐵", "🐷", "🐷", "🐶", "🐶", "🐸", "🐸", 
-                "🐟", "🐟", "🐞", "🐞", "🐔", "🐔", "🦇", "🦇"];
+const emojis = ["😀 ", "😀 ", "😁", "😁", "🤣", "🤣", "😇", "😇", 
+                "😍", "😍", "😘", "😘", "😝", "😝", "😡", "😡"];
 
 let openCards = [];
 
 // responsavel por embaralhar
-let shuffleEmojis = emojis.sort(() => (Math.random()> 0.5 ? 2 : -1));
-
+let shuffleEmojis = emojis.sort(() => (Math.random()> 0.5) ? 2 : -1);
 
 for (let i=0; i < emojis.length; i++)
 {
@@ -18,6 +17,45 @@ for (let i=0; i < emojis.length; i++)
     //pegando o emoji
     box.innerHTML = shuffleEmojis[i];
 
+    //Quando da o click
+    box.onclick = handleClick;
+
     // criando o "filho"
     document.querySelector(".game").appendChild(box);
+}
+
+// responsavel pelo click
+function handleClick(){
+
+    //serve para armazena as carta
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen"); //adiciona classe boxOpen, entao vai mostra a carta
+        openCards.push(this); // vai adiciona na nossa lista
+    }
+
+    //verifica se duas carta selecionado, caso seja verdade, verifica se eles se correspodem 
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+//Serve para verirca se deu match as duas carta
+function checkMatch(){
+    //caso deu martch
+    if (openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch"); // vai adiciona class boxMatch
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen"); //vai adiciona classe boxOpen que vai revirar a carta
+        openCards[1].classList.remove("boxOpen");
+    }
+    
+    //tipo zera a nossa lista
+    openCards = [];
+
+    //condiçao de vitoria
+    //  a quantidade de class que tem vo valor .boxMAtCh corresponde a quantida de emoji
+    if (document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Parabéns, você venceu!")
+    }
 }
